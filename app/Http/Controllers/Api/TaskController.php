75e8d485e08abe87ce2015deda849cd576b8331d
userprasanna <prasanna.chats@gmail.com>
2022-03-18 11:37:47 +0800

    refactor

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -7,20 +7,18 @@ use Illuminate\Http\Request;
 use App\Task;
 use App\History;
 
+define("STATUS_200", 200);
+define("STATUS_404", 400);
+define("STATUS_201", 201);
+
 class TaskController extends Controller
 {
     public function createTask(Request $request) {
-
-        if (Task::where('key', $request->key)->exists()) {
-            $result = Task::where('key', $request->key)->first()->toArray();
-            $history = new History;
-            $history->key = $result['key'];
-            $history->value = $result['value'];
-            $history->datetime = $result['datetime'];
-            $history->save();
-
+        $message = "Record has been created";
+        $result = Task::where('key', $request->key)->first()->toArray();
+        if(!empty($result)){
+            $this->history($result);
             $task = Task::where('key',$result['key'])->update(['value'=>$request->value,'datetime'=>strtotime(date("Y-m-d H:i:s"))]);
-
             $message = "Record has been updated";
         }else{
             $task = new Task;
@@ -28,21 +26,21 @@ class TaskController extends Controller
             $task->value = $request->value;
             $task->datetime = strtotime(date("Y-m-d H:i:s"));
             $task->save();
-            $message = "Record has been created";
         }
 
       return response()->json([
         "message" => $message
-      ], 201);
+      ], STATUS_201);
     }
 
     public function getAllTasks() {
       $tasks = Task::get(['key','value','datetime'])->toJson(JSON_PRETTY_PRINT);
-      return response($tasks, 200);
+      $status = (!empty($tasks)) ? STATUS_200 : STATUS_400;
+      return response($tasks, $status);
     }
 
     public function getTask($key) {
-      if (Task::where('key', $key)->exists()) {
+        $task = [];
         $cond['key'] = $key;
         if(isset($_GET['timestamp'])){
             $cond['datetime'] = trim($_GET['timestamp']);
@@ -51,16 +49,15 @@ class TaskController extends Controller
         if(empty($task) && isset($_GET['timestamp'])){
             $task = History::where($cond)->get(['key','value','datetime'])->toArray();
         }
-        if(!empty($task)){
-            foreach($task as $k => $val){
-                $task[$k]['pretty_time'] = date('Y-m-d h:i A',$val['datetime']);
-            }
-        }
-        return response($task, 200);
-      } else {
-        return response()->json([
-          "message" => "record not found"
-        ], 404);
-      }
+        $status = (!empty($task)) ? STATUS_200 : STATUS_400;
+        return response()->json(array("data"=>$task)),$status);
+    }
+
+    public function createHistory(){
+        $history = new History;
+        $history->key = $result['key'];
+        $history->value = $result['value'];
+        $history->datetime = $result['datetime'];
+        $history->save();
     }
 }
