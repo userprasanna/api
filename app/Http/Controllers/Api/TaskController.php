c2dc2b0baa179309f20b1d17f7b55e158c042d29
userprasanna <prasanna.chats@gmail.com>
2022-03-18 15:17:58 +0800

    add test cases

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -8,39 +8,43 @@ use App\Task;
 use App\History;
 
 define("STATUS_200", 200);
-define("STATUS_404", 400);
-define("STATUS_201", 201);
+define("STATUS_404", 404);
+define("STATUS_400", 400);
 
 class TaskController extends Controller
 {
     public function createTask(Request $request) {
-        $message = "Record has been created";
-        $result = Task::where('key', $request->key)->first()->toArray();
-        if(!empty($result)){
-            $this->history($result);
-            $task = Task::where('key',$result['key'])->update(['value'=>$request->value,'datetime'=>strtotime(date("Y-m-d H:i:s"))]);
-            $message = "Record has been updated";
-        }else{
-            $task = new Task;
-            $task->key = $request->key;
-            $task->value = $request->value;
-            $task->datetime = strtotime(date("Y-m-d H:i:s"));
-            $task->save();
+
+        $validator = \Validator::make($request->all(), [
+            'key' => 'required|string|max:255',
+            'value' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(array("error"=>"input request error"),STATUS_400);
         }
 
-      return response()->json([
-        "message" => $message
-      ], STATUS_201);
+        $result = Task::where('key', $request->key)->first();
+        if(!empty($result)){
+            $result = $result->toArray();
+            $this->createHistory($result);
+            $task = Task::where('key',$request->key)->update(['value'=>$request->value,'datetime'=>strtotime(date("Y-m-d H:i:s"))]);
+        }else{
+            $this->addTask($request);
+        }
+      return $this->getTask($request->key);
     }
 
     public function getAllTasks() {
-      $tasks = Task::get(['key','value','datetime'])->toJson(JSON_PRETTY_PRINT);
-      $status = (!empty($tasks)) ? STATUS_200 : STATUS_400;
-      return response($tasks, $status);
+      $tasks = Task::get(['key','value','datetime']);
+      return response()->json(array("data"=>$tasks), (!empty($tasks)) ? STATUS_200 : STATUS_404);
     }
 
     public function getTask($key) {
-        $task = [];
+        if (!$key || is_numeric($key)) {
+            return response()->json(array("error"=>"input request error"),STATUS_400);
+        }
+
         $cond['key'] = $key;
         if(isset($_GET['timestamp'])){
             $cond['datetime'] = trim($_GET['timestamp']);
@@ -49,15 +53,23 @@ class TaskController extends Controller
         if(empty($task) && isset($_GET['timestamp'])){
             $task = History::where($cond)->get(['key','value','datetime'])->toArray();
         }
-        $status = (!empty($task)) ? STATUS_200 : STATUS_400;
-        return response()->json(array("data"=>$task)),$status);
+        $status = (!empty($task)) ? STATUS_200 : STATUS_404;
+        return response()->json(array("data"=>$task),$status);
     }
 
-    public function createHistory(){
+    public function createHistory($result){
         $history = new History;
         $history->key = $result['key'];
         $history->value = $result['value'];
         $history->datetime = $result['datetime'];
         $history->save();
+    }
+
+    public function addTask($request){
+        $task = new Task;
+        $task->key = $request->key;
+        $task->value = $request->value;
+        $task->datetime = strtotime(date("Y-m-d H:i:s"));
+        $task->save();
     }
 }
--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -2,48 +2,127 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class TaskTest extends TestCase
 {
-    use WithFaker, RefreshDatabase;
+    use WithFaker,RefreshDatabase;
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function testgetAllRecords()
+    public function testgetAllRecordsNull()
     {
         $response = $this->get('api/tasks/get_all_records');
+        $response->assertStatus(200)->assertJsonStructure([
+                'data' => [
+                    '*' => []
+                ]
+            ]);
+    }
 
-        $response->assertStatus(200);
+    public function testgetTaskByUnknownKey()
+    {
+        $response = $this->get('api/tasks/'.$this->faker->name);
+        $response->assertStatus(404)->assertJsonStructure([
+                'data' => [
+                    '*' => []
+                ]
+            ]);
+    }
+
+    public function testgetTaskByNumbers()
+    {
+        $response = $this->get('api/tasks/'.$this->faker->unique()->numberBetween(1, 200));
+        $response->assertStatus(400)
+            ->assertJsonStructure(['error']);
+    }
+
+    public function testShowForMissingParam() {
+        $payload = [
+            'value' => "test"
+        ];
+        $this->json('post', "api/tasks")
+             ->assertStatus(400)
+             ->assertJsonStructure(['error']);
+        
     }
 
     public function testgetTaskByKey()
     {
-        $response = $this->get('api/tasks/dummydata');
+        $key = $this->faker->name;
+        $payload = [
+            'key' => $key,
+            'value' => $this->faker->name,
+        ];
+        $create = $this->post('api/tasks',$payload);
+        $response = $this->get('api/tasks/'.$key);
+        $response->assertStatus(200)->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'key',
+                        'value',
+                        'datetime'
+                    ]
+                ]
+            ]);
+    }
 
-        $response->assertStatus(404);
+    public function testcreateTaskByMissingParam()
+    {
+        $payload = [
+            'key' => $this->faker->name
+        ];
+        $this->json('post', 'api/tasks', $payload)
+        ->assertStatus(400)
+             ->assertJsonStructure(['error']);
+    }
+
+    public function testcreateTaskByInvalidParamType()
+    {
+        $payload = [
+            'key' => $this->faker->unique()->numberBetween(1, 200),
+            'value'=>$this->faker->name
+        ];
+        $this->json('post', 'api/tasks', $payload)
+        ->assertStatus(400)
+             ->assertJsonStructure(['error']);
     }
 
     public function testcreateTask()
     {
-        $data = [
+        $payload = [
             'key' => $this->faker->name,
             'value' => $this->faker->name,
         ];
-        $response = $this->post('api/tasks', $data);
-
-        $response->assertStatus(201);
+        $this->json('post', 'api/tasks', $payload)
+        ->assertStatus(200)
+        ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'key',
+                        'value',
+                        'datetime'
+                    ]
+                ]
+            ]);
+        $this->assertDatabaseHas('tasks', $payload);
     }
+
     public function testlistRecords()
     {
         $response = $this->get('api/tasks/get_all_records');
-
         $response->assertStatus(200)->assertJsonStructure([
-                '*' => ['key','value','datetime'],
+                'data' => [
+                    '*' => [
+                        'key',
+                        'value',
+                        'datetime'
+                    ]
+                ]
             ]);
     }
 }
